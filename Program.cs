a28286a246a6374d56761d124a2b34c6cb2266c9
Evgeiy_B <bocharovevgen@gmail.com>
2015-07-13 16:43:46 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            double ans = 1;
+            Console.WriteLine("Enter a number: ");
+            number = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                ans *= i+1;
+            }
+            Console.WriteLine("{0}! = {1}", number, ans);
+            Console.ReadKey();
+            
+
+        }
+    }
+}
